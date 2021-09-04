26d2b8bbe2c1cb482d6d81db8bac35e2a222cec4
scorpiotzh <835598264@qq.com>
2021-09-04 16:40:59 +0800

    etcd kv

--- etcd/kv.go
+++ etcd/kv.go
@@ -14,6 +14,11 @@ func (c *Client) GetKV(k string, opts ...clientv3.OpOption) (*clientv3.GetRespon
 	return kv.Get(c.ctx, k, opts...)
 }
 
+func (c *Client) DelKV(k string, opts ...clientv3.OpOption) (*clientv3.DeleteResponse, error) {
+	kv := clientv3.NewKV(c.client)
+	return kv.Delete(c.ctx, k, opts...)
+}
+
 func StringPutResponse(resp *clientv3.PutResponse) {
 	if resp.PrevKv != nil {
 		log.Info("put prev: ", string(resp.PrevKv.Key), string(resp.PrevKv.Value))
@@ -25,6 +30,13 @@ func StringPutResponse(resp *clientv3.PutResponse) {
 func StringGetResponse(resp *clientv3.GetResponse) {
 	log.Info("get len:", len(resp.Kvs))
 	for _, v := range resp.Kvs {
+		log.Info(string(v.Key), string(v.Value))
+	}
+}
+
+func StringDeleteResponse(resp *clientv3.DeleteResponse) {
+	log.Info("del len:", len(resp.PrevKvs), resp.Deleted)
+	for _, v := range resp.PrevKvs {
 		log.Info(string(v.Key), string(v.Value))
 	}
 }
--- etcd/kv_test.go
+++ etcd/kv_test.go
@@ -3,34 +3,69 @@ package etcd
 import (
 	"context"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/scorpiotzh/toolib"
 	"testing"
 )
 
-func TestKV(t *testing.T) {
-	c, err := NewClient(context.Background(), []string{"http://127.0.0.1:2379"})
+func getClient() (*Client, error) {
+	return NewClient(context.Background(), []string{"http://127.0.0.1:2379"})
+}
+
+func TestPutKV(t *testing.T) {
+	c, err := getClient()
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
-	if resp, err := c.PutKV("/test/t3", "1234"); err != nil {
-		panic(err)
+	if resp, err := c.PutKV("/test/t1", "1234"); err != nil {
+		t.Fatal(err)
 	} else {
 		StringPutResponse(resp)
 	}
 	if resp, err := c.PutKV("/test/t2", "3214"); err != nil {
-		panic(err)
+		t.Fatal(err)
 	} else {
 		StringPutResponse(resp)
 	}
+	if resp, err := c.PutKV("/test/t3", "32145"); err != nil {
+		t.Fatal(err)
+	} else {
+		StringPutResponse(resp)
+	}
+}
 
+func TestGetKV(t *testing.T) {
+	c, err := getClient()
+	if err != nil {
+		t.Fatal(err)
+	}
 	if resp, err := c.GetKV("/test/t1"); err != nil {
-		panic(err)
+		t.Fatal(err)
 	} else {
 		StringGetResponse(resp)
 	}
 
 	if resp, err := c.GetKV("/test/", clientv3.WithPrefix()); err != nil {
-		panic(err)
+		t.Fatal(err)
 	} else {
 		StringGetResponse(resp)
+	}
+}
+
+func TestDelKV(t *testing.T) {
+	c, err := getClient()
+	if err != nil {
+		panic(err)
+	}
+	if resp, err := c.DelKV("test"); err != nil {
+		t.Fatal(err)
+	} else {
+		log.Info("resp:", toolib.JsonString(resp))
+		StringDeleteResponse(resp)
+	}
+	if resp, err := c.DelKV("/test/t2", clientv3.WithPrefix()); err != nil {
+		t.Fatal(err)
+	} else {
+		log.Info("resp:", toolib.JsonString(resp))
+		StringDeleteResponse(resp)
 	}
 }
